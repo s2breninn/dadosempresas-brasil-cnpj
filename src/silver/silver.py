a4ada21eab5181f19ce3eb717b24214692971c47
Breno Mendes <b.mendesbernardo2020@gmail.com>
2024-05-23 20:58:10 -0300

    perf: change file reading structure from CSV to Parquet

--- src/silver/silver.py
+++ src/silver/silver.py
@@ -9,6 +9,14 @@ class DataProcessor:
         self.connection = self.connect_database()
         self.json_data = json_data
         self.extracted_folder = os.path.join(base_directory, 'extracted')
+        self.parquet_folder = os.path.join(base_directory, 'silver.parquet')
+
+    def create_folder(self, folder_path: str) -> None:
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            print(f'Diretório criado em: {folder_path}')
+        else:
+            print(f'Diretório já existe: {folder_path}')
 
     def connect_database(self):
         try:
@@ -24,35 +32,42 @@ class DataProcessor:
         except duckdb.Error as e:
             print(f'Erro ao criar tabela {table_name}: {e}')
 
+    def csv_to_parquet(self, dir_path_full_data_csv: str, columns: list, path_file_name_parquet: str):
+        query = f"""
+            COPY (
+                SELECT * FROM read_csv_auto(
+                    '{dir_path_full_data_csv}',
+                    normalize_names=true,
+                    ignore_errors=true,
+                    delim=';',
+                    header=true,
+                    names={columns}
+                    )
+            ) TO '{path_file_name_parquet}' (FORMAT PARQUET)
+        """
+        try:
+            self.connection.execute(query)
+            print(f"Convertido {dir_path_full_data_csv} CSV para PARQUET: {self}")
+        except duckdb.Error as e:
+            print(f'Erro ao processar arquivo {dir_path_full_data_csv}: {e}')
+
     def process_files(self):
         for root, dirs, _ in os.walk(self.extracted_folder):
             for dir in dirs:
+
                 table_info = self.json_data['tabelas'].get(dir)
+
                 if table_info:
                     table_name, columns = table_info[0]['nome'], table_info[0]['colunas']
-                    dir_path_full_data = os.path.join(root, dir, '*')
-                    self.process_file(dir_path_full_data, columns)
+                    dir_path_full_data_csv = os.path.join(root, dir, '*')
 
-    def process_file(self, file_path: str, columns: list):
-        query = f"""
-            SELECT *
-            FROM read_csv_auto(
-                '{file_path}',
-                normalize_names=true,
-                ignore_errors=true,
-                delim=';',
-                header=true,
-                names={columns}
-            )
-            LIMIT 20
-        """
-        try:
-            result_df = self.connection.execute(query).df()
-            print(f"\nDados de {file_path}:\n", result_df)
-        except duckdb.Error as e:
-            print(f'Erro ao processar arquivo {file_path}: {e}')
+                    file_name_parquet = f"{table_name}.parquet"
+                    path_file_name_parquet = os.path.join(self.parquet_folder, file_name_parquet)
+
+                    self.csv_to_parquet(dir_path_full_data_csv, columns, path_file_name_parquet)
 
     def process_data(self):
+        self.create_folder(self.parquet_folder)
         self.process_files()
 
 if __name__ == '__main__':
